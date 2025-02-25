ba7370f02400ada3f44c7536ef837359e842c14a
Nayem783982 <majedulislam223311@gmail.com>
2025-02-25 20:11:36 +0600

    add new style

---
+++ javascript/addmoney.js
@@ -0,0 +1,20 @@
+document.getElementById('addMoney')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const amount = document.getElementById("amount").value;
+        const convertedAmount = parseFloat(amount);
+
+        const pin = document.getElementById("pin").value;
+        const convertedPin = parseInt(pin);
+
+        const mainBalance = document.getElementById("mainBalance").innerText;
+        const convertedMainBalance = parseFloat(mainBalance);
+
+        if( convertedPin === 1234){
+            const sum = convertedMainBalance + convertedAmount;
+            document.getElementById("mainBalance").innerText = sum;
+            document.getElementById("amount").value = '';
+        }else{
+            console.log("jaito noi");
+        }
+    })
---
+++ javascript/btn.js
@@ -0,0 +1,16 @@
+document.getElementById("login-btn")
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const accountNumber = document.getElementById('account-number').value;
+        const pin = document.getElementById('pin').value;
+        const convartedPIn = parseInt(pin);
+        if ( accountNumber.length === 11){
+           if(convartedPIn === 1234){
+                window.location.href = "../main.html";
+           }else{
+            alert('Your pin is wrong!Please enter valid PIN');
+           }
+        }else{
+            alert("Please enter valid ACCOUNT NUMBER");
+        }
+    })
