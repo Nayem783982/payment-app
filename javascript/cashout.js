69428bd7f4d62cd0f4ccc31eccac8a9f752be97d
Nayem783982 <majedulislam223311@gmail.com>
2025-02-26 14:56:58 +0600

    fix new bug

--- javascript/cashout.js
+++ javascript/cashout.js
@@ -1,22 +1,30 @@
-document.getElementById("cashout-btn")
+document.getElementById("Cash-Out-btn")
     .addEventListener('click', function (event) {
         event.preventDefault();
+
+        const agentNumber = document.getElementById('agent-number').value;
+
         const cashOutAmount = document.getElementById("cash-out-amount").value;
         const convertedCashOutAmount = parseFloat(cashOutAmount);
 
-        const pin = document.getElementById("pin").value;
+        const pin = document.getElementById("pin1").value;
         const convertedPin = parseInt(pin);
 
         const mainBalance = document.getElementById("mainBalance").innerText;
         const convertedMainBalance = parseFloat(mainBalance);
 
-        if( convertedPin === 1234){
-            const sub = convertedMainBalance - convertedCashOutAmount;
-            document.getElementById("mainBalance").innerText = sub;
-            document.getElementById("amount").value = '';
+        if(agentNumber.length === 11 ){
+            if(convertedPin === 1234){
+                const sub = convertedMainBalance - convertedCashOutAmount;
+                document.getElementById("mainBalance").innerText = sub;
+                document.getElementById("cash-out-amount").value = '';
+                document.getElementById("pin1").value = '';
+                document.getElementById('agent-number').value = '';
+            }
+            else{
+                alert("Enter valid PIN !");
+            }
         }else{
-            alert("enter valid PIN !")
+            alert("Enter valid AGENT-NUMBER !");
         }
-
-
 })
